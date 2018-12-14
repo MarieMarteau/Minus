645a47e591bc082c7a95e05482224ff5876eb1af
crespinb <45688504+crespinb@users.noreply.github.com>
2018-12-14 17:05:05 +0100

    Add files via upload

--- corps.js
+++ corps.js
@@ -7,36 +7,45 @@ function getBody() {
 //					   new THREE.Vector2(-0.5,2.5),
 //					   new THREE.Vector2(0,3)]);
     const baseShape = new THREE.Shape();
-    baseShape.lineTo(-1,1);
-    baseShape.lineTo(0,3);
+//    baseShape.lineTo(-1,1);
+//    baseShape.lineTo(0,2);
+//    baseShape.lineTo(2.5,2.5);
+//    baseShape.lineTo(0,3);
+    const array = [new THREE.Vector2(0,0),
+		   new THREE.Vector2(-1,1),
+		   new THREE.Vector2(-0.25,2),
+		   new THREE.Vector2(-0.5,2.5),
+		   new THREE.Vector2(0,3)];
+    baseShape.splineThru(array);
     //baseShape.lineTo(0,0);
     const circleCurve = new THREE.EllipseCurve(0,0,0.5,0.5);
     //const circlePath = new THREE.Path(circleCurve.getPoints(100));
     //const circlePath = new THREE.Path();
     //circlePath.absellipse(0,0,0.5,0.5,0,2*Math.PI,false,Math.PI/2);
-    let bodyPoints = []
+    let bodyPointsBuffer1 = []
+    let bodyPointsBuffer2 = []
+    let bodyGeometry = new THREE.Geometry();
     const shapePoints = baseShape.getPoints();
+//ATTENTION : il faudra s'assurer que shapePoints est trie par coord. "y" croissante pour ne pas planter les calculs d'enveloppe convexe (ici garanti par la definition de array)
     const maxI = 100
     const circleRadius = 0.5
     for(let j=0; j<shapePoints.length; j++) {
 	if(shapePoints[j].x == 0) {
-	    bodyPoints.push(new THREE.Vector3(shapePoints[j].x, shapePoints[j].y, 0))
+	    bodyPointsBuffer2.push(new THREE.Vector3(shapePoints[j].x, shapePoints[j].y, 0))
 	}
-    }
-    for(let i=0; i<maxI; i++) {
-	for(let j=0; j<shapePoints.length; j++) {
-	    if(shapePoints[j].x != 0) {
-		bodyPoints.push(new THREE.Vector3(circleRadius*Math.cos(2*i*Math.PI/maxI)*shapePoints[j].x, shapePoints[j].y, circleRadius*Math.sin(2*i*Math.PI/maxI)*shapePoints[j].x));
+	else {
+	    for(let i=0; i<maxI; i++) {
+		bodyPointsBuffer2.push(new THREE.Vector3(circleRadius*Math.cos(2*i*Math.PI/maxI)*shapePoints[j].x, shapePoints[j].y, circleRadius*Math.sin(2*i*Math.PI/maxI)*shapePoints[j].x));
 	    }
 	}
+	if(bodyPointsBuffer1.length != 0) {
+	    const partialGeom = new THREE.ConvexGeometry(bodyPointsBuffer1.concat(bodyPointsBuffer2));
+	    bodyGeometry.merge(partialGeom);
+	}
+	bodyPointsBuffer1 = bodyPointsBuffer2.slice();
+	bodyPointsBuffer2 = [];
     }
-    console.log(bodyPoints);
-    const bodyGeometry = new THREE.ConvexGeometry(bodyPoints);
-    bodyGeometry.vertices = bodyPoints;
-    const hull = new THREE.QuickHull();
-    hull.setFromPoints(bodyPoints);
-    const faces = hull.faces();
-    console.log(faces);
+    //const bodyGeometry = new THREE.ConvexGeometry(bodyPoints);
     //const circlePath = new THREE.CatmullRomCurve3(points);
     //console.log(circlePath);
     //const bodyGeometry = new THREE.ExtrudeGeometry(baseShape, {extrudePath:circlePath, steps:10, bevelEnabled:false})//, {extrudePath: circleCurve});
