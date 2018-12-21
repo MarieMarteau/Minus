af97e256ecaa5c03c854b324f5abcad105248172
crespinb <45688504+crespinb@users.noreply.github.com>
2018-12-21 14:29:08 +0100

    Add files via upload

--- aile.js
+++ aile.js
@@ -1,14 +1,25 @@
 "use strict";
 
 function creationSurfaces(verticesArray, material) {
+    const width = 0.1;
     const l = verticesArray.length;
-    if(l<2) return;
-    const vertices = verticesArray.flatten();
-    const geometry=new THREE.Geometry();
-    geometry.vertices = vertices;
-    triangles = THREE.ShapeUtils.triangulateShape(vertices, holes);
-    for(let i=0; i<triangles.length; i++) {
-	geometry.faces.push(new THREE.Face3(triangles[i][0], triangles[i][1], triangles[i][2]));
+    if(l<2) return new THREE.Mesh();
+    const geometry = new THREE.Geometry();
+    for(let i=0; i<l-1; i++) {
+	const firstLine = verticesArray[i];
+	const secondLine = verticesArray[i+1];
+	for(let j=0; j<firstLine.length-1; j++) {
+	    const array = [firstLine[j],
+			   firstLine[j+1],
+			   secondLine[j],
+			   secondLine[j+1],
+			   new THREE.Vector3(firstLine[j].x, firstLine[j].y, firstLine[j].z+width),
+			   new THREE.Vector3(firstLine[j+1].x, firstLine[j+1].y, firstLine[j+1].z+width),
+			   new THREE.Vector3(secondLine[j].x, secondLine[j].y, secondLine[j].z+width),
+			   new THREE.Vector3(secondLine[j+1].x, secondLine[j+1].y, secondLine[j+1].z+width)];
+	    const partialGeom = new THREE.ConvexGeometry(array);
+	    geometry.merge(partialGeom);
+	}
     }
     const mesh = new THREE.Mesh(geometry, material);
     return mesh;
